0429ea72361d865d0b196aaa000e958679bada4b
Yulia778 <84283264+Yulia778@users.noreply.github.com>
2021-05-26 17:52:03 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    getNewDate(31,12,2011);}
+        public static void getNewDate(int day, int month, int year){
+            int[] daysInMonth = {-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            int nextDay = day;
+            int nextMonth = month;
+            int nextYear = year;
+            if (day == daysInMonth[month]) {
+                nextDay = 1;
+                nextMonth++;
+                if (month == 12) {
+                    nextMonth = 1;
+                    nextYear++;
+                }
+
+            } else {
+                nextDay++;
+            }
+            System.out.println(nextDay + "." + nextMonth + "." + nextYear);
+        }
+
+
+    }
